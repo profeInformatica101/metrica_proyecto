caad271a619f0479de4fb9344fd1d0245f9f3a6c
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-05-15 09:43:58 +0200

    Inicio proyecto. Estructura creada.

---
+++ GymApp/src/main/java/com/instituto/gymapp/GymApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.instituto.gymapp;
+
+/**
+ *
+ * @author usuarioDAW
+ */
+public class GymApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
